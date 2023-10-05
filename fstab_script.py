7694dcc266e495b91f51b1ea895b643f4a1047c4
cmrnwlsh <109775729+cmrnwlsh@users.noreply.github.com>
2023-10-05 14:42:41 -0400

    Update fstab_script.py

--- fstab_script.py
+++ fstab_script.py
@@ -2,7 +2,7 @@ from subprocess import run
 
 blkid = run(["blkid"], capture_output=True, text=True)
 ids = {
-    line.split(" ")[0][:-1]: line.split(" ")[1].replace('"', "")
+    line.split()[0][:-1]: line.split()[1].replace('"', "")
     for line in blkid.stdout.splitlines()
     if line[:4] == "/dev"
 }
