118cebe87c99b5d0c096c28d54259c971fa3cdc5
cameron <cmrnwlsh@gmail.com>
2023-10-05 12:42:03 -0400

    commit

---
+++ fstab_script.py
@@ -0,0 +1,15 @@
+from subprocess import run
+
+blkid = run(["blkid"], capture_output=True, text=True)
+ids = {
+    line.split(" ")[0][:-1]: line.split(" ")[1].replace('"', "")
+    for line in blkid.stdout.splitlines()
+    if line[:4] == "/dev"
+}
+
+with open("./fstab_test", "r+") as fstab:
+    text = fstab.read()
+    for dev in ids:
+        text = text.replace(dev, ids[dev])
+    fstab.seek(0)
+    fstab.write(text)
